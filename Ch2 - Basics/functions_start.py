fb3fa7e5cacdf9b5da9f1c9093f2afba5c6e66ee
Mani <chidvilasballa@gmail.com>
2023-06-29 19:58:47 -0400

    mac commits

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -5,17 +5,26 @@
 
 
 # TODO: define a basic function
-
+def basic():
+    print("I am a basic function")
 
 # TODO: function that takes arguments
+def argfunction(a, b):
+    print(a + b)
 
-
+argfunction(1,5)
 # TODO: function that returns a value
+def retfunction(a,b):
+    return a+b
 
-
+print("2 + 3 =", retfunction(2,3))
 # TODO: function with default value for an argument
-
+ 
+def power(num, x=1): #x takes 1 as default when no argument is passed by the user.
+        result = 1
+        for i in range(x):
+            result*=num
+        return result
+print(power(5))
 
 # TODO: function with variable number of arguments
-
-
--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -7,21 +7,28 @@
 # Basic data types in Python: Numbers, Strings, Booleans, Sequences, Dictionaries
 myint = 5
 myfloat = 13.2
-mystr = "This is a string"
+mystr = "This is a global string"
 mybool = True
 mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 myset = {"a", 1, 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
-print(myset)
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
+# print(myset)
+
+print(mylist[0:5:2]) #prints every 2nd element from 0 to 5
+print(mylist[::-1]) #prints the list in reverse order
+print(mydict["one"]) #prints the value of the key "one" in the dictionary
+
+print("string type" + str(123)) #string can concatenate with string only
+
 # re-declaring a variable works
 
 # to access a member of a sequence type, use []
@@ -36,3 +43,9 @@ print(myset)
 
 # Global vs. local variables in functions
 
+def someFunction():
+    mystr = "This is a local string"
+    print(mystr)
+
+someFunction() #prints the local variable mystr
+print(mystr) #prints the global variable mystr    
